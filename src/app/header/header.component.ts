e64c6b87e7929af4fc17e05a442be8cd3a1fa497
szuhaydv <szuhaydv@gmail.com>
2024-05-29 20:49:32 +0200

    initial color scheme

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -22,4 +22,21 @@ export class HeaderComponent {
   hideDropdown() {
     this.isDropdownActive = false
   }
+
+  lightMode = true
+
+  updateColorScheme() {
+    if (this.lightMode) {
+      document.documentElement.style.setProperty('--clr-black-white', '#000')
+      document.documentElement.style.setProperty('--clr-background', '#181823')
+      document.documentElement.style.setProperty('--clr-header', '#210062')
+      document.documentElement.style.setProperty('--clr-bnw-inverse', '#fff')
+    } else {
+      document.documentElement.style.setProperty('--clr-black-white', '#fff')
+      document.documentElement.style.setProperty('--clr-background', '#ebf6f7')
+      document.documentElement.style.setProperty('--clr-header', '#28282B')
+      document.documentElement.style.setProperty('--clr-bnw-inverse', '#000')
+    }
+    this.lightMode = !this.lightMode
+  }
 }

b80860330301ed505ed40b08bf9ed402a4223384
szuhaydv <szuhaydv@gmail.com>
2024-05-30 10:44:16 +0200

    aside section + routing start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SkillsComponent } from './skills/skills.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'skills', component: SkillsComponent},
+  { path: '', component: MainComponent}
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -16,7 +17,7 @@ export class HeaderComponent {
   decoratorOffset = 0
 
   ngOnInit() {
-    this.calculateDecoratorOffset(0);
+    this.calculateDecoratorOffset(this.decoratorOffset);
   }
 
   calculateDecoratorOffset(id: number) {

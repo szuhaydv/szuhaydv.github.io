6ff4bba963bd3ef3e9569da866cdc2843d45de3e
szuhaydv <szuhaydv@gmail.com>
2024-05-31 19:44:38 +0200

    filter options

---
+++ src/app/projects/filterClasses.ts
@@ -0,0 +1,56 @@
+export class Frameworks {
+  frameworks: Framework[]
+
+  constructor(frameworks: Framework[]) {
+    this.frameworks = frameworks
+  }
+
+  set isChecked(id: number) {
+    if (this.frameworks[id].isChecked) {
+      this.frameworks[id].isChecked = false
+      return
+    }
+    for (const framework of this.frameworks) {
+      framework.isChecked = false
+    }
+    this.frameworks[id].isChecked = true
+  }
+}
+
+export class Framework {
+  id: number
+  title: string
+  isChecked = false
+
+  constructor(id: number, title: string) {
+    this.id = id
+    this.title = title
+  }
+}
+
+export class Capability {
+  id: number
+  title: string
+  isChecked = false
+
+  constructor(id: number, title: string) {
+    this.id = id
+    this.title = title
+  }
+}
+
+export class Project {
+  id: number
+  image: string
+  title: string
+  description: string
+  isDeployed: boolean
+
+  constructor(id: number, image: string, title: string, description: string, isDeployed: boolean) {
+    this.id = id
+    this.image = image
+    this.title = title
+    this.description = description
+    this.isDeployed = isDeployed
+  }
+}
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CountUpDirective } from '../count-up.directive';
+import { Frameworks, Framework, Capability } from './filterClasses';
 
 
 @Component({
@@ -10,6 +11,67 @@ import { CountUpDirective } from '../count-up.directive';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+  // textColor = 'black'
+  // toggleTextColor() {
+  //   this.textColor = this.textColor == 'black' ? 'transparent' : 'black'
+  // }
+  showFrameworks = false
+  isAnyFrameworkSelected() {
+    for (const framework of this.frameworks.frameworks) {
+      if (framework.isChecked) {
+        return true
+      }
+    }
+    return false
+  }
+  toggleFrameworks() {
+    this.showFrameworks = !this.showFrameworks
+  }
+  leaveFrameworks() {
+    if (this.showFrameworks) {
+      this.showFrameworks = false
+    }
+  }
+
+  frameworks = new Frameworks([
+      new Framework(0, "React"),
+      new Framework(1, "Angular"),
+      new Framework(2, "Mobile")
+    ]
+  )
+  capabilities = [
+    new Capability(0, "API calls"),
+    new Capability(0, "FullStack"),
+    new Capability(1, "Database"),
+    new Capability(2, "Auth"),
+    new Capability(3, "SEO"),
+    new Capability(4, "Deployed"),
+  ]
+  showCapabilites = false
+  isAnyCapabilitySelected() {
+    for (const capability of this.capabilities) {
+      if (capability.isChecked) {
+        return true
+      }
+    }
+    return false
+  }
+  toggleCapabilites() {
+    this.showCapabilites = !this.showCapabilites
+  }
+  leaveCapabilities() {
+    if (this.showCapabilites) {
+      this.showCapabilites = false
+    } 
+  }
+
+  selectFramework(id: number) {
+    this.frameworks.isChecked = id
+  }
+
+  selectCapability(id: number) {
+    this.capabilities[id].isChecked = !this.capabilities[id].isChecked
+  }
 
   codingCount = 1000
   techCount = 40

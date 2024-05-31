094e47a9394bcc29b3d54aa720c6993cdfd92d5f
szuhaydv <szuhaydv@gmail.com>
2024-05-31 10:11:43 +0200

    projects basic layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { MinimalLayoutComponent } from './minimal-layout/minimal-layout.component';
 import { FullLayoutComponent } from './full-layout/full-layout.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
   { path: 'skills', component: MinimalLayoutComponent},
-  { path: '', component: FullLayoutComponent}
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'about', component: FullLayoutComponent},
+  { path: 'contact', component: FullLayoutComponent},
+  { path: '', component: FullLayoutComponent},
 ];
--- src/app/minimal-layout/skills/skills.component.ts
+++ src/app/minimal-layout/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { AnimationState, KeyRotation } from './animationStates.enum';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
@@ -13,52 +13,87 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       state('framework', style({ transform: 'translate(-45%, -40%)' })),
       state('backend', style({ transform: 'translate(-10%, -35%)' })),
       state('tooling', style({ transform: 'translate(-7.5%, -70%)' })),
-      transition('* <=> *', [animate('0.3s 0.1s ease-in-out')]),
+      transition('* <=> *', [animate('1s 0.1s cubic-bezier(0.36, 0, 0.66, -0.56)')]),
     ]),
+    trigger('showButton', [
+      state('show', style({ opacity: '1' })),
+      state('hide', style({ opacity: '0' })),
+      transition('show => hide', [animate('0s 0s linear')]),
+      transition('hide => show', [animate('0.3s 2s ease-in-out')]),
+    ])
   ],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
-export class SkillsComponent {
+export class SkillsComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    this.showButton = 'show'
+  }
+
+  hideButton() {
+    this.showButton = 'hide'
+  }
+
+  showAgain() {
+    if (this.showButton == 'hide') {
+      this.showButton = 'show'
+    }
+  }
 
   currentState = AnimationState.Initial
+  showButton = 'hide'
+  buttonRotation = 'rotate(0)'
+  containerAlignment = 'flex-end'
   
   stepForward() {
     switch (this.currentState) {
       case AnimationState.Initial:
         this.currentState = AnimationState.WebDevTools
+        this.buttonRotation = 'rotate(90deg)'
+        this.hideButton()
         break
       case AnimationState.WebDevTools:
         this.currentState = AnimationState.Frameworks
+        this.buttonRotation = 'rotate(180deg)'
+        this.containerAlignment = 'flex-start'
+        this.hideButton()
         break
       case AnimationState.Frameworks:
         this.currentState = AnimationState.Backend
+        this.buttonRotation = 'rotate(90deg)'
+        this.hideButton()
         break
       case AnimationState.Backend:
         this.currentState = AnimationState.Tooling
+        this.buttonRotation = 'rotate(-90deg)'
+        this.hideButton()
         break
       case AnimationState.Tooling:
+        this.currentState = AnimationState.Initial
+        this.buttonRotation = 'rotate(0)'
+        this.containerAlignment = 'flex-end'
+        this.hideButton()
         break
     }
   }
 
-  stepBackwards() {
-    switch (this.currentState) {
-      case AnimationState.Tooling:
-        this.currentState = AnimationState.Backend;
-        break;
-      case AnimationState.Backend:
-        this.currentState = AnimationState.Frameworks;
-        break;
-      case AnimationState.Frameworks:
-        this.currentState = AnimationState.WebDevTools;
-        break;
-      case AnimationState.WebDevTools:
-        this.currentState = AnimationState.Initial;
-        break;
-      case AnimationState.Initial:
-        break;
-    }
-  }
+  // stepBackwards() {
+  //   switch (this.currentState) {
+  //     case AnimationState.Tooling:
+  //       this.currentState = AnimationState.Backend;
+  //       break;
+  //     case AnimationState.Backend:
+  //       this.currentState = AnimationState.Frameworks;
+  //       break;
+  //     case AnimationState.Frameworks:
+  //       this.currentState = AnimationState.WebDevTools;
+  //       break;
+  //     case AnimationState.WebDevTools:
+  //       this.currentState = AnimationState.Initial;
+  //       break;
+  //     case AnimationState.Initial:
+  //       break;
+  //   }
+  // }
 
 }

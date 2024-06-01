a88b14479e9a19eeb05dca12c14490f66bddfcc2
szuhaydv <szuhaydv@gmail.com>
2024-06-01 15:20:50 +0200

    initial about section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { MinimalLayoutComponent } from './minimal-layout/minimal-layout.component';
 import { FullLayoutComponent } from './full-layout/full-layout.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   { path: 'skills', component: MinimalLayoutComponent},
   { path: 'projects', component: ProjectsComponent},
-  { path: 'about', component: FullLayoutComponent},
-  { path: 'contact', component: FullLayoutComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
   { path: '', component: FullLayoutComponent},
 ];

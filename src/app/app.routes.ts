237d6055cfcb716cc61fdc2064fc449892854824
szuhaydv <szuhaydv@gmail.com>
2024-06-02 20:15:32 +0200

    project details basic layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { FullLayoutComponent } from './full-layout/full-layout.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 export const routes: Routes = [
   { path: 'skills', component: MinimalLayoutComponent},
   { path: 'projects', component: ProjectsComponent},
+  { path: 'projects/:id', component: ProjectDetailsComponent },
   { path: 'about', component: AboutComponent},
   { path: 'contact', component: ContactComponent},
   { path: '', component: FullLayoutComponent},

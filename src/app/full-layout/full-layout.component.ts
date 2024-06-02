7c576f65c80ea82632fba00ccf004062570b5424
szuhaydv <szuhaydv@gmail.com>
2024-06-02 14:15:19 +0200

    aside hover animation

--- src/app/full-layout/full-layout.component.ts
+++ src/app/full-layout/full-layout.component.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { MainComponent } from './main/main.component';
-import { trigger, transition, style, animate } from '@angular/animations';
+import { trigger, transition, style, animate, sequence, state } from '@angular/animations';
 @Component({
   selector: 'app-full-layout',
   standalone: true,
@@ -8,17 +8,44 @@ import { trigger, transition, style, animate } from '@angular/animations';
   templateUrl: './full-layout.component.html',
   styleUrl: './full-layout.component.css',
   animations: [
-    trigger('buttonHover', [
-      transition(':hover', [
-        animate('0.1s ease-in-out', style({ transform: 'rotate(5deg)' })),
-        animate('0.1s ease-in-out', style({ transform: 'rotate(-5deg)' })),
-        animate('0.1s ease-in-out', style({ transform: 'rotate(5deg)' })),
-        animate('0.1s ease-in-out', style({ transform: 'rotate(-5deg)' })),
-        animate('0.1s ease-in-out', style({ transform: 'rotate(0deg)' })),
+    trigger('buzz', [
+      state('initial', style({ transform: 'rotate(0)'})),
+      state('buzzed', style({ transform: 'rotate(0)'})),
+      transition('initial => buzzed', [
+          style({ transform: 'scale(1.1)' }),
+          animate('0.1s ease-in-out', style({ transform: 'scale(1.1) rotate(5deg)' })),
+          animate('0.1s ease-in-out', style({ transform: 'scale(1.1) rotate(-5deg)' })),
+          animate('0.1s ease-in-out', style({ transform: 'scale(1.1) rotate(5deg)' })),
+          animate('0.1s ease-in-out', style({ transform: 'scale(1.1) rotate(-5deg)' })),
+          animate('0.1s ease-in-out', style({ transform: 'scale(1.1) rotate(0deg)' })),
       ]),
     ]),
   ],
 })
 export class FullLayoutComponent {
+  githubState = 'initial'
+  linkedinState = 'initial'
+  gmailState = 'initial'
+
+
+  githubHover() {
+    this.githubState = 'buzzed'
+  }
+  gmailHover() {
+    this.gmailState = 'buzzed'
+  }
+  linkedinHover() {
+    this.linkedinState = 'buzzed'
+  }
+  githubHoverEnded() {
+    this.githubState = 'initial'
+  }
+  linkedinHoverEnded() {
+    this.linkedinState = 'initial'
+  }
+  gmailHoverEnded() {
+    this.gmailState = 'initial'
+  }
+
 
 }

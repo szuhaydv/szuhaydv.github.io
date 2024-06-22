f99a18740d25aa7994636f418009aeacd2001c15
szuhaydv <szuhaydv@gmail.com>
2024-06-22 20:33:46 +0200

    footer copyright notice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RouterLink, RouterLinkActive, AsideComponent],
+  imports: [RouterOutlet, HeaderComponent, RouterLink, RouterLinkActive, AsideComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
